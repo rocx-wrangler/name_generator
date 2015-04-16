c43414a78f61a296dade0c529674e988b66014ce
rocx-wrangler <rocx_wrangler@yahoo.com>
2015-04-16 17:29:45 +0530

    chinese.py

---
+++ chinese.py
@@ -0,0 +1,16 @@
+#!/home/fds/freeware/python26/bin/python
+#generate Chinese names randomly
+
+from random import randint
+
+vowel = ['a', 'i', 'o', 'u']
+alpha = ['g', 'h', 'j', 'l', 'm', 'n', 'q', 'w', 'x', 'y', 'z']
+suffix = ['ng', 'zhou', 'ping', 'iang', 'eng']
+for i in range(1,10):
+    name = ''
+    name = alpha[randint(0,len(alpha)-1)]
+    if name == 'z':
+        name = name+ 'h'
+    name = name + vowel[randint(0,len(vowel)-1)] + suffix[randint(0,len(suffix)-1)]
+
+    print name
