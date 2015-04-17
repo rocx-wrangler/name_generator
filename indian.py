c6460861ad880a144f8767958c5daaa68e5fc484
Git Cat <rocx_wrangler@yahoo.com>
2015-04-17 13:58:06 +0530

    Update indian.py
    Fixing closing braces/parenthesis

--- indian.py
+++ indian.py
@@ -10,5 +10,5 @@ suffix = ['nu', 'ni']
 
 for i in range(1,10):
     name = ''
-    name = prefix[randint(0,len(prefix)-1)] + vowel[randint(0,len(vowel)-1)] + suffix[randint(0,len(suffix)-1)]
+    name = prefix[randint(0,len(prefix))-1] + vowel[randint(0,len(vowel))-1] + suffix[randint(0,len(suffix))-1]
     print name
