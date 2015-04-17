1a17b9e5797e314882d50f06ec74608b0ee1672b
Git Cat <rocx_wrangler@yahoo.com>
2015-04-17 13:57:31 +0530

    Update chinese.py
    Fixing closing braces/parenthesis

--- chinese.py
+++ chinese.py
@@ -8,9 +8,9 @@ alpha = ['g', 'h', 'j', 'l', 'm', 'n', 'q', 'w', 'x', 'y', 'z']
 suffix = ['ng', 'zhou', 'ping', 'iang', 'eng', 'hua', 'ing']
 for i in range(1,10):
     name = ''
-    name = alpha[randint(0,len(alpha)-1)]
+    name = alpha[randint(0,len(alpha))-1]
     if name == 'z':
         name = name+ 'h'
-    name = name + vowel[randint(0,len(vowel)-1)] + suffix[randint(0,len(suffix)-1)]
+    name = name + vowel[randint(0,len(vowel))-1] + suffix[randint(0,len(suffix))-1]
 
     print name
