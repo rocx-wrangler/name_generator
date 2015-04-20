3d49f9192b7214b3fff2b0e070ca14323cdbfe54
Git Cat <rocx_wrangler@yahoo.com>
2015-04-20 19:45:29 +0530

    Update indian.py

--- indian.py
+++ indian.py
@@ -1,10 +1,9 @@
 #!/home/fds/freeware/python26/bin/python
-
 from random import randint
- 
-prefix = ['Ram', 'Nag', 'Har', 'Gyan', 'Karam', 'Mohan', 'Mah','Sur','Shiv', 'Nah', 'Dharam', 'Dhyan']
+
+prefix = ['Ram', 'Nag', 'Har', 'Gyan', 'Karam', 'Mohan', 'Gan','Kam', 'Sur','Shiv', 'Dharam', 'Dhyan']
 suffix = ['chand', 'das', 'charan', 'prasad', 'raj', 'esh' ]
- 
+
 for i in range(1,10):
     name = ''
     name = prefix[randint(0,len(prefix))-1] + suffix[randint(0,len(suffix))-1]
