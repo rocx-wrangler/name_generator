8cf86850530fe5eb8891a722f50f6d9e222fca12
Git Cat <rocx_wrangler@yahoo.com>
2015-04-20 19:25:55 +0530

    Update indian.py
    Making Indian names more realistic

--- indian.py
+++ indian.py
@@ -1,14 +1,11 @@
 #!/home/fds/freeware/python26/bin/python
 
-#Generate indian names randomly
-
 from random import randint
-
-prefix = ['b', 'l', 'm','n','s', 't']
-vowel = ['a', 'i', 'o', 'u']
-suffix = ['nu', 'ni']
-
-for i in range(1,10):
+ 
+prefix = ['Ram', 'Nag', 'Har', 'Gyan', 'Karam', 'Mohan', 'Mah','Sur','Shiv', 'Nah', 'Dharam', 'Dhyan']
+suffix = ['chand', 'das', 'charan', 'prasad', 'raj', 'esh' ]
+ 
+for i in range(1,100):
     name = ''
-    name = prefix[randint(0,len(prefix))-1] + vowel[randint(0,len(vowel))-1] + suffix[randint(0,len(suffix))-1]
+    name = prefix[randint(0,len(prefix))-1] + suffix[randint(0,len(suffix))-1]
     print name
